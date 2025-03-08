e36c97edcbdd8d0b0fe3f66e8d30e172ea3ce098
RidvanOzturk <ozturkridvan1@gmail.com>
2025-03-08 17:26:38 +0300

    fluent validation is currently controlling the lunch break.

--- SmartAppointmentSystem.Api/Models/Validators/AppointmentValidator.cs
+++ SmartAppointmentSystem.Api/Models/Validators/AppointmentValidator.cs
@@ -7,8 +7,13 @@ public class AppointmentValidator : AbstractValidator<AppointmentRequestModel>
     public AppointmentValidator()
     {
         RuleFor(x => x.Status).NotEmpty().NotNull();
-        RuleFor(x=>x.DoctorId).NotEmpty().NotNull();
-        RuleFor(x => x.Time).NotNull().Must(time => time > DateTime.Now);
-        RuleFor(x => x.Notes).Null().Empty();
+        RuleFor(x => x.DoctorId).NotEmpty().NotNull();
+        RuleFor(x => x.Time)
+            .NotNull()
+            .Must(x => x > DateTime.Now)
+            .WithMessage("The appointment time must be in the future.")
+            .Must(x => !(x.TimeOfDay >= new TimeSpan(12, 0, 0) && x.TimeOfDay < new TimeSpan(13, 0, 0)))
+            .WithMessage("Appointments cannot be scheduled between 12:00 and 13:00.");
+        RuleFor(x => x.Notes).Empty(); 
     }
 }
